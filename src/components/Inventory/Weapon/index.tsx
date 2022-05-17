e663bee09a2e83949291d2f48345e97d2f0677dd
Dave Connolly <daveconnolly90@gmail.com>
2022-05-17 20:56:31 +0100

    Allowed for 2 slot weapons

--- src/components/Inventory/Weapon/index.tsx
+++ src/components/Inventory/Weapon/index.tsx
@@ -8,10 +8,11 @@ interface WeaponProps {
     quantity: number;
     weaponId: number;
     attachmentId: number;
+    colSpan: number;
 }
 
-const Weapon: React.FC<WeaponProps> = ({ quantity, weaponId, attachmentId }) => (
-    <GridItem w="100%" h="24" bg="#1d1e1e" color="#ffffff" position="relative" borderRadius="5px" p="1px" border="2px solid #fff">
+const Weapon: React.FC<WeaponProps> = ({ quantity, weaponId, attachmentId, colSpan }) => (
+    <GridItem w="100%" h="24" bg="#1d1e1e" color="#ffffff" position="relative" borderRadius="5px" p="1px" border="2px solid #fff" colSpan={colSpan}>
         <img src={selectWeaponImage(weaponId, attachmentId)} className="weapon" />
         <div className="quantity">{quantity}</div>
     </GridItem>
